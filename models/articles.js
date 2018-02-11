a83bf0e20054cb9ea67e5aff0baf5d07187231b4
michellechristina <michelle@thisisbam.com>
2018-02-11 11:33:20 -0500

    models set up

---
+++ models/articles.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new LibrarySchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  // `name` must be of type String
+  // `name` must be unique, the default mongoose error message is thrown if a duplicate value is given
+  headline: {
+    type: String,
+    unique: true
+  },
+  summary: {
+    type: String,
+    unique: true
+  },
+  url: {
+    type: String,
+    unique: true
+  },
+  // `books` is an array that stores ObjectIds
+  // The ref property links these ObjectIds to the Book model
+  // This allows us to populate the Library with any associated Books
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comments"
+    }
+  ]
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Articles = mongoose.model("Articles", ArticleSchema);
+
+// Export the Library model
+module.exports = Articles;
--- server.js
+++ server.js
@@ -14,12 +14,42 @@ var db = require("./models");
 // Initialize Express
 var app = express();
 
- 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.render('home');
+ // handlebars
+ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+ app.set('view engine', 'handlebars');
+  
+ app.get('/', function (req, res) {
+     res.render('home');
+ });
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/scraper", {
+  useMongoClient: true
 });
+
+// When the server starts, create and save a new Article document to the db
+// The "unique" rule in the Library model's schema will prevent duplicate libraries from being added to the server
+db.Articles
+  .create({ name: "Articles" })
+  .then(function(dbArticles) {
+    // If saved successfully, print the new Library document to the console
+    console.log(dbArticles);
+  })
+  .catch(function(err) {
+    // If an error occurs, print it to the console
+    console.log(err.message);
+  });
+
+
  
-app.listen(3000);
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
